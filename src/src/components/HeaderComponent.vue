cf05eeda8b5e324b905846f0d4106817ba8f1ae7
A-Yeong <dkdud064@kumoh.ac.kr>
2025-01-07 15:07:01 +0900

    프로젝트 설정

---
+++ src/src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <TopUtilComponent/>
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import "../assets/css/HeaderComponent.css";
+import TopUtilComponent from "./TopUtilComponent.vue";
+
+export default defineComponent({
+    name: 'HeaderComponent',
+    components: {
+        TopUtilComponent
+    }
+})
+</script>
