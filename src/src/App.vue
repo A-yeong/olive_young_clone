97f8141220fa4c01e09ee8e5c179bd32b4a210f6
A-Yeong <dkdud064@kumoh.ac.kr>
2025-01-07 15:40:43 +0900

    TopUtil

--- src/src/App.vue
+++ src/src/App.vue
@@ -6,6 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import "../src/assets/css/App.css"
 import HeaderComponent from './components/HeaderComponent.vue';
 
 export default defineComponent({
@@ -15,14 +16,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
