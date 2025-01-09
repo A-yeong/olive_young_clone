d5686420856fa3f2fd764669b60c08e6931b63ef
A-Yeong <dkdud064@kumoh.ac.kr>
2025-01-09 21:27:58 +0900

    add: 로고, 검색창 ui

---
+++ src/src/components/HeaderInnerComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header_inner">
+            <h1>
+                <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0">
+                    <img src="../assets/img/logo.png">
+                </a>
+            </h1>
+            <div class="search_box" id="w_search_box">
+                <input type="hidden" name="chkButton" id="chkButton" value>
+                <input type="text" id="query" class="header_search_input" placeholder="상품, 브랜드, 성분 검색" autocomplete="off">
+                <button class="btn_delete" id="search_delete" style="display: none;">검색어 삭제제</button>
+                <button id="searchSubmit">검색</button>
+            </div>
+        </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import "../assets/css/HeaderInnerComponent.css";
+
+export default defineComponent({
+    name: "HeaderInnerComponent",
+    components: {}
+});
+</script>
---
+++ src/src/components/TopUtilComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="top_util">
+            <ul class="menu_list" id="menu_list_header">
+                <li class="join">
+                    <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0&t_page=%EB%A1%9C%EA%B7%B8%EC%9D%B8&t_click=%ED%97%A4%EB%8D%94&t_header_type=%EB%A1%9C%EA%B3%A0">회원가입</a>
+                </li>
+                <li class="login">
+                    <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0&t_page=%EB%A1%9C%EA%B7%B8%EC%9D%B8&t_click=%ED%97%A4%EB%8D%94&t_header_type=%EB%A1%9C%EA%B3%A0">로그인</a>
+                </li>
+                <li class="cart">
+                    <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0&t_page=%EB%A1%9C%EA%B7%B8%EC%9D%B8&t_click=%ED%97%A4%EB%8D%94&t_header_type=%EB%A1%9C%EA%B3%A0">장바구니</a>
+                </li>
+                <li class="order">
+                    <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0&t_page=%EB%A1%9C%EA%B7%B8%EC%9D%B8&t_click=%ED%97%A4%EB%8D%94&t_header_type=%EB%A1%9C%EA%B3%A0">주문배송</a>
+                </li>
+                <li class="customer">
+                    <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0&t_page=%EB%A1%9C%EA%B7%B8%EC%9D%B8&t_click=%ED%97%A4%EB%8D%94&t_header_type=%EB%A1%9C%EA%B3%A0">고객센터</a>
+                </li>
+                <li class="store">
+                    <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0&t_page=%EB%A1%9C%EA%B7%B8%EC%9D%B8&t_click=%ED%97%A4%EB%8D%94&t_header_type=%EB%A1%9C%EA%B3%A0">올영매장</a>
+                </li>
+                <li class="global">
+                    <a href="https://www.oliveyoung.co.kr/store/main/main.do?oy=0&t_page=%EB%A1%9C%EA%B7%B8%EC%9D%B8&t_click=%ED%97%A4%EB%8D%94&t_header_type=%EB%A1%9C%EA%B3%A0">Global</a>
+                </li>
+            </ul>
+        </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import "../assets/css/TopUtilComponent.css";
+
+export default defineComponent({
+    name: "TopUtilComponent",
+    components: {}
+})
+</script>
