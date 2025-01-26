9b0bdeaf04d0505f32047d1cd5070c868b3681f3
A-Yeong <dkdud064@kumoh.ac.kr>
2025-01-26 20:00:27 +0900

    feat: slick button

--- src/src/components/ContainerComponent.vue
+++ src/src/components/ContainerComponent.vue
@@ -3,14 +3,14 @@
         <div id="Contents">
             <div class = "main_full_banner">
                 <div class="banner_wrap slick_slider slick-initialized slick-slider" id="mainFullSlider">
-                    <button type="button" data-role = "none" class="slick-prev slick-arrow" aria-label="Previous" role="button" style="display: none;">이전 슬라이드</button>
+                    <button type="button" data-role = "none" class="slick-prev slick-arrow" aria-label="Previous" role="button" style="display: block;">이전 슬라이드</button>
                     <div aria-live="polite" class="slick-list draggable">
                         <div class="slick-track" role="listbox" style="opacity: 1; width: 19524px;"></div>
                     </div>
-                    <button type="button" data-role = "none" class="slick-next slick-arrow" aria-label="Next" role="button" style="display: none;">다음 슬라이드</button>
+                    <button type="button" data-role = "none" class="slick-next slick-arrow" aria-label="Next" role="button" style="display: block;">다음 슬라이드</button>
                     <button id="mainSliderBtn">멈춤</button>
                     <p class="slick-paging">
-                        <span class="cur_no">number</span>/12
+                        <span class="cur_no">0</span>/12
                     </p>
                 </div>
             </div>
@@ -21,11 +21,12 @@
 <script lang="ts">
 import {defineComponent} from "vue";
 import SlickSlideComponent from "./SlickSlideComponent.vue";
+import "../assets/css/ContainerComponent.css"
 
 export default defineComponent({
     name: "ContainerComponent",
     components: {
-        SlickSlideComponent
+        
     }
 });
 </script>
