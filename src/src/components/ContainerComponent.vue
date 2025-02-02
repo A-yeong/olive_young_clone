c6f5a83215c7a77943d05dd90f031e74b314496d
A-Yeong <dkdud064@kumoh.ac.kr>
2025-02-02 21:42:26 +0900

    component 분리

--- src/src/components/ContainerComponent.vue
+++ src/src/components/ContainerComponent.vue
@@ -5,12 +5,21 @@
                 <div class="banner_wrap slick_slider slick-initialized slick-slider" id="mainFullSlider">
                     <button type="button" data-role = "none" class="slick-prev slick-arrow" aria-label="Previous" role="button" style="display: block;">이전 슬라이드</button>
                     <div aria-live="polite" class="slick-list draggable">
-                        <div class="slick-track" role="listbox" style="opacity: 1; width: 19524px;"></div>
+                        <div class="slick-track" role="listbox" style="opacity: 1; width: 19524px;">
+                            <SlickSlideComponent
+                                v-for="(slide, index) in slides"
+                                :key="index"
+                                :imageSrc="slide.image"
+                                :title="slide.title"
+                                :description="slide.description"
+                                :isActive="currentIndex === index"
+                                :index="index"/>
+                        </div>
                     </div>
                     <button type="button" data-role = "none" class="slick-next slick-arrow" aria-label="Next" role="button" style="display: block;">다음 슬라이드</button>
                     <button id="mainSliderBtn">멈춤</button>
                     <p class="slick-paging">
-                        <span class="cur_no">0</span>/12
+                        <span class="cur_no">{{ currentIndex + 1 }}</span>/{{ slides.length }}
                     </p>
                 </div>
             </div>
@@ -19,14 +28,43 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { slides, defineComponent, ref, computed, onMounted, onUnmounted } from "vue";
 import SlickSlideComponent from "./SlickSlideComponent.vue";
 import "../assets/css/ContainerComponent.css"
 
 export default defineComponent({
     name: "ContainerComponent",
     components: {
-        
+        SlickSlideComponent
+    },
+    setup() {
+        const slides = ref([
+            {
+                image: "../assets/img/slick01.jpg",
+                title: "올영PICK",
+                description: "올리브영이 PICK한\n이 달의 브랜드\n4일 특가 & 올리브 키트 증정"
+            },
+            {
+                image: "../assets/img/slick02.jpg",
+                title: "올영PICK",
+                description: "올리브영이 PICK한\n이 달의 브랜드\n4일 특가 & 올리브 키트 증정"
+            },
+            {
+                image: "../assets/img/slick03.jpg",
+                title: "올영PICK",
+                description: "올리브영이 PICK한\n이 달의 브랜드\n4일 특가 & 올리브 키트 증정"
+            },
+            {
+                image: "../assets/img/slick04.jpg",
+                title: "올영PICK",
+                description: "올리브영이 PICK한\n이 달의 브랜드\n4일 특가 & 올리브 키트 증정"
+            },
+            {
+                image: "../assets/img/slick05.jpg",
+                title: "올영PICK",
+                description: "올리브영이 PICK한\n이 달의 브랜드\n4일 특가 & 올리브 키트 증정"
+            },
+        ])
     }
 });
 </script>
--- src/src/components/SlickSlideComponent.vue
+++ src/src/components/SlickSlideComponent.vue
@@ -1,6 +1,19 @@
 <template>
-    <div class="slick-slide" data-slick-index = {{ num }} aria-hidden="true" style="" tabindex="-1" role="option" aria-describedby="">
-        <a onclick= "" href=""></a>
+    <div class="slick-slide" data-slick-index="1" aria-hidden="true" style="width: 1627px; position: relative; left: -4881px; top: 0px; z-index: 998; opacity: 0; transition: opacity 500ms;" tabindex="-1" role="option" aria-describedby="slick-slide01">
+        <a onclick= "" href="" class="banner_link">
+            <p class="banner_desc">
+                <span class="tit" style="color: #fff;">올영PICK</span>
+                <strong style="color: #fff;">
+                    올리브영이
+                    <br>
+                    PICK한
+                    <br>
+                    이 달의 브랜드
+                </strong>
+                <span style="color: #fff;">4일 특가 & 올리브 키트 증정</span>
+            </p>
+            <img src="../assets/img/slick01.jpg" alt="올리브영키트 4일 특가">
+        </a>
     </div>
 </template>
 
